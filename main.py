47c7e93870bd46d5cb78b75c13dfbbddef880e06
Daniel Solano <dasolano@bancolombia.com.co>
2022-07-07 14:59:16 -0500

    create PathContent class

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Path:
 
 
 
-class FilesExtractor:
+class PathContent:
 
   def __init__(self, path_instance):
     self.path_instance = path_instance
@@ -37,17 +37,58 @@ class FilesExtractor:
       raise TypeError('El argumento ingresado no es de tipo Path')
     self._path_instance = path_instance
 
+  @property
+  def content_structure(self):
+    path_str = self.path_instance.path
+    return PathContent._build_structure(path_str)
 
-class ExcelFilesMerger(FilesExtractor):
+  @staticmethod
+  def _build_structure(path_str):
+    content = []
+    for path in os.listdir(path_str):
+      relative_path_str = f'{path_str}/{path}'
+      is_dir = os_path.isdir(relative_path_str)
+      last_modification_date = os_path.getmtime(relative_path_str)
+      size = os_path.getsize(relative_path_str)
 
-  ALLOWED_EXTENSIONS = ('xlsx',)
+      path_info = {'path': relative_path_str, 'is_dir': is_dir, 'last_mod_date': last_modification_date, 'size': size}
+      
+      if is_dir:
+        path_info.update({'content': PathContent._build_structure(relative_path_str)})
 
-  def __init__(self, path_instance):
-    super().__init__(path_instance)
+      content.append(path_info)
 
+    return content
+
+print(PathContent(Path('.')).content_structure)
+
+
+# class ExcelFilesMerger:
+
+#   ALLOWED_EXTENSIONS = ('xlsx',)
+
+#   def __init__(self, path_instance):
+#     super().__init__(path_instance)
   
-  def merge_sheets(self):
+#   def merge_sheets(self):
+#     pass
+
+#   @staticmethod
+#   def recursive_search(path):
     
+#     if os_path.isfile(path):
+#       print(path)
+#     elif os_path.isdir(path):
+#       for dir_path in os.listdir(path):
+#         print(dir_path)
+#         ExcelFilesMerger.recursive_search(dir_path)
+
+#   def paths(self):
+#     path = self.path_instance.path
+
+#     ExcelFilesMerger.recursive_search(path)
+  
+# ExcelFilesMerger(Path('./')).paths()
 
   
 
